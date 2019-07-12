8d36c51245f9688a272fce3f934c46ce66ee3fc3
olivierpicault <olivier.picault@gmail.com>
2019-07-12 22:59:59 +0200

    basic setup

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.css$/, use: [ 'vue-style-loader', 'css-loader' ] }
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new VueLoaderPlugin(),
+    // Hot reload
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
