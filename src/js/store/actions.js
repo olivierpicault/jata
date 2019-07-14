93812c7e9d78189b4c214eece081b61653d65f20
olivierpicault <olivier.picault@gmail.com>
2019-07-14 20:44:14 +0200

    reorder todos

--- src/js/store/actions.js
+++ src/js/store/actions.js
@@ -1,19 +1,23 @@
+import moment from 'moment'
 import db from './../db'
 
 const actions = {
   async create (context, todo) {
+    todo.created = moment().format()
     await db.collection('todos').add(todo)
     context.dispatch('read')
   },
   async read (context) {
     const todos = []
     await db.collection('todos')
+      .orderBy('created', 'desc')
       .get()
       .then(snapshot => {
         snapshot.forEach(doc => {
-          todos.splice(0, 0, {
+          todos.push({
             id: doc.id,
-            content: doc.data().content
+            content: doc.data().content,
+            created: doc.data().created
           })
         })
       })
